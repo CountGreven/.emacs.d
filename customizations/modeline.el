f3b4d3fd0eb07a9c800a09a116a4e48a200e9005
greven <you@example.com>
2018-09-24 17:03:16 +0200

    cleaning up

--- customizations/modeline.el
+++ customizations/modeline.el
@@ -13,25 +13,11 @@
 
 ;; Setup fancy icons for git
 
-(defun -custom-modeline-github-vc ()
-  (let ((branch (mapconcat 'concat (cdr (split-string vc-mode "[:-]")) "-")))
-    (concat
-     (propertize (format " %s" (all-the-icons-alltheicon "git")) 'face `(:height 1.2) 'display '(raise -0.1))
-     " · "
-     (propertize (format "%s" (all-the-icons-octicon "git-branch"))
-                 'face `(:height 1.3 :family ,(all-the-icons-octicon-family))
-                 'display '(raise -0.1))
-     (propertize (format " %s" branch) 'face `(:height 0.9)))))
+(defadvice vc-mode-line (after strip-backend () activate)
+  (when (stringp vc-mode)
+    (let ((gitlogo (replace-regexp-in-string "^ Git." ":" vc-mode)))
+          (setq vc-mode gitlogo))))
 
-(defun -custom-modeline-svn-vc ()
-  (let ((revision (cadr (split-string vc-mode "-"))))
-    (concat
-     (propertize (format " %s" (all-the-icons-faicon "cloud")) 'face `(:height 1.2) 'display '(raise -0.1))
-     (propertize (format " · %s" revision) 'face `(:height 0.9)))))
+;; Battery indicator
 
-(defun custom-modeline-icon-vc ()
-  (when vc-mode
-    (cond
-      ((string-match "Git[:-]" vc-mode) (-custom-modeline-github-vc))
-      ((string-match "SVN-" vc-mode) (-custom-modeline-svn-vc))
-      (t (format "%s" vc-mode)))))
+
